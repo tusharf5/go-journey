4a123bcba110c8e21c55477467b9201c3339ad65
Tushar Sharma <ts17995@gmail.com>
2021-06-13 14:20:07 +0530

    add comments

--- ds/max-repeat-char.go
+++ ds/max-repeat-char.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	str := "asmnaksdnkansdaslkdnkasjhdkjhsadkjakhdbkasndkasdkkkkkkkkkkkkkajhajkdhasjkdkashdjkashdjkashdjkashdkjashdjkaaaaaaaa"
+	str := "aabbccccbbddddeeeefnnghmpqswkkkkwookmkskmkqkqwwmkks"
 	fmt.Println(max_repeat_char(str))
 }
 
@@ -19,9 +19,9 @@ func max_repeat_char(chars string) string {
 
 	// 3 things could happen
 
-	// the new element has no effect on the maximum sub list as adding it does not make a new maximum
-	// it creates a new maximum sublist ending at the last element which is this newly added
-	// in some cases the new element is the maximum itself
+	// Case 1 - the new element has no effect on the maximum sub list as adding it does not make a new maximum
+	// Case 2 - it creates a new maximum sublist ending at the last element which is this newly added
+	// Case 3 - in some cases the new element is the maximum itself
 
 	max_repeat := ""
 
@@ -30,15 +30,18 @@ func max_repeat_char(chars string) string {
 	for i := 0; i < len(chars); i++ {
 
 		if i != 0 && chars[i] == chars[i-1] && chars[i] == local_max_repeat[0] {
-			// this will increase the local max
+			// Case 2 check
 			local_max_repeat = local_max_repeat + string(chars[i])
 		} else {
+			// Case 3 check
 			local_max_repeat = string(chars[i])
 		}
 
 		if len(local_max_repeat) > len(max_repeat) {
 			max_repeat = local_max_repeat
-		}
+		} // else {
+		// Case 1
+		//	}
 
 	}
 
